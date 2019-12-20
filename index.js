2fdda32b14d6d151c8b405c880cf60cd99a0b201
manuelbarzi@gmail.com <Manuel@MacBook-Pro-de-Manuel.local>
2019-12-20 10:23:37 +0300

    update

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const http = require('http')
 const url = require('url')
 
-function add(method, path, handler) {
-	((this.handlers || (this.handlers = {}) ) && (this.handlers[method] || (this.handlers[method] = {})))[path] = handler
-}
-
-function get(path, handler) {
-	this.add('GET', path, handler)
-}
-
-function post(path, handler) {
-	this.add('POST', path, handler)
-}
-
 module.exports = function() {
+	const handlers = {}
+
 	const server = http.createServer((req, res) => {
 		const { method } = req
 		const { pathname: path, query } = url.parse(req.url, true)
@@ -22,12 +12,20 @@ module.exports = function() {
 
 		let _handler
 
-		(server.handlers && (_handler = server.handlers[method]) && (_handler = _handler[path])) && (_handler(req, res) || true) || res.end(`Cannot ${method} on ${path}`)
+		((_handler = handlers[method]) && (_handler = _handler[path])) && (_handler(req, res) || true) || res.end(`Cannot ${method} on ${path}`)
 	})
 
-	server.add = add
-	server.get = get
-	server.post = post
+	server.add = function(method, path, handler) {
+		(handlers[method] || (handlers[method] = {}))[path] = handler
+	}
+
+	server.get = function(path, handler) {
+		this.add('GET', path, handler)
+	}
+
+	server.post = function(path, handler) {
+		this.add('POST', path, handler)
+	}
 
 	return server
 }
